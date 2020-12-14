6fb72ca749d0bd62ca2ce7166f32743fb426739c
Kamil Kasprzyk <k.kasprzyk2@gmail.com>
2020-12-14 16:06:51 +0100

    Initial commit for Api Gateway

---
+++ java/api-gateway/src/main/java/com/digitalcrafting/gateway/controller/ProxyController.java
@@ -0,0 +1,14 @@
+package com.digitalcrafting.gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gateway/api/proxy/v1")
+public class ProxyController {
+    @GetMapping("/")
+    public String test() {
+        return "I'm alive";
+    }
+}
