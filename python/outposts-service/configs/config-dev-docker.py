035ae0c7e69d90876f9f6747ac0b37669ce6856b
DigitalCrafting <k.kasprzyk2@gmail.com>
2021-10-06 13:55:13 +0200

    Removed multi language services

--- python/outposts-service/configs/config-dev-docker.py
+++
@@ -1,7 +0,0 @@
-CONFIG: dict = {
-    'discoveryIp': 'the-one-discovery',
-    'discoveryPort': '8081',
-    'appName': 'outposts',
-    'appIp': 'the-one-outposts-service',
-    'appPort': '8084'
-}
--- python/outposts-service/configs/config-dev-local.py
+++
@@ -1,7 +0,0 @@
-CONFIG: dict = {
-    'discoveryIp': 'localhost',
-    'discoveryPort': '8081',
-    'appName': 'outposts',
-    'appIp': 'localhost',
-    'appPort': '8084'
-}
